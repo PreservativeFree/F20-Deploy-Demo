3492be171afd0cf44292e8cde99e4718a8402a56
Nelson Moreno <thegreyabove@gmail.com>
2022-04-06 11:51:02 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const PORT = processs.env.PORT || 4005
+
+app.listen(PORT, () => { console.log(`Listening on ${PORT}`)})
+
+//server setup
