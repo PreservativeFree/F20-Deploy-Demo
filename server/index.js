edcacc76c10513cfcd775c0464e80fb77b1f3bc7
Nelson Moreno <thegreyabove@gmail.com>
2022-04-06 13:04:42 -0400

    fifth commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static('public')) //summons main and styles.css in public
 
 
 app.get('/', (req, res) => {
